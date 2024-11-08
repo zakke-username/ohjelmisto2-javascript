23441e02354e6997ea627aba4eca6d2f54765b21
zakke-username <zakkezweyg@gmail.com>
2024-11-08 09:55:13 +0200

    t4 muokkaus

--- module1/4/4.js
+++ module1/4/4.js
@@ -1,17 +1,4 @@
 let name = prompt('What is your name?');
-
-let randomInt = Math.floor(Math.random() * 4);
-
-let output = name + ', you are ';
-
-if (randomInt === 0) {
-  output += 'Gryffindor';
-} else if (randomInt === 1) {
-  output += 'Ravenclaw';
-} else if (randomInt === 2) {
-  output += 'Hufflepuff';
-} else if (randomInt === 3) {
-  output += 'Slytherin';
-}
-
-document.querySelector('#house').innerHTML = output;
+let houses = ['Gryffindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin'];
+let house = houses[Math.floor(Math.random() * houses.length)];
+document.querySelector('#house').innerHTML = `${name}, you are ${house}`;
